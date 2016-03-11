7dd31cbde075e9f7af17a64593b81723449d8880
Manjesh Vinayaka <manjesh@quetzal.in>
2016-03-11 13:05:14 +0530

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,27 @@
 #! /bin/bash
 
-git remote add dokku dokku@cloud.quezx.com:ci
-git push dokku master
+
+echo "Changing Directory to ./dist"
+cd dist
+
+echo "Setting git config user.email and user.name"
+git config --global user.email "manjesh@quetzal.in"
+git config --global user.name "Manjesh V"
+
+echo "Git Init"
+git init
+
+echo "Adding remote"
+git remote add origin https://github.com/manjeshpv/circleci-test.dist
+
+echo "Adding files to git"
+git add *
+
+echo "Commiting..."
+git commit -m "Todo: comming Message"
+
+echo "Started deploying"
+git push origin master --force
+
+echo "Pushed to git successfully Successfully!"
+exit 0
